42a1cbb1820b715ce778c1b062be718c6464762e
rohitcsa <rohitcsa.rg@gmail.com>
2017-11-15 18:42:31 +0530

    first

---
+++ src/main/java/com/quiz/mailQuizReport.java
@@ -0,0 +1,53 @@
+package com.quiz;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * Servlet implementation class mailQuizReport
+ */
+public class mailQuizReport extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public mailQuizReport() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		System.out.println("Reached here!");
+
+		JsonParser parser = new JsonParser();
+		JsonArray jArr_QAKey = parser.parse(request.getParameter("QA_Key")).getAsJsonArray();
+
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
